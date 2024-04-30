ab06386285e53c084d7827e2b11cdbc60794a91f
escdaniel <102396163+escdaniel@users.noreply.github.com>
2024-04-30 09:35:19 -0500

    Add files via upload
    aqui el archivo metaverso para trabajar con sus respectivas dependencias

---
+++ proyecto_metaverso/proyecto_metaverso/src/test/java/com/riwi/proyecto_metaverso/ProyectoMetaversoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.proyecto_metaverso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMetaversoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
