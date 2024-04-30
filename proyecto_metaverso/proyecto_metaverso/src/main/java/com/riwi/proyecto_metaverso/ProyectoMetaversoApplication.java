1d5c9f7801e01963a442881de72a1f1df86e0563
daniel <102396163+escdaniel@users.noreply.github.com>
2024-04-30 09:44:36 -0500

    01

---
+++ proyecto_metaverso/proyecto_metaverso/src/main/java/com/riwi/proyecto_metaverso/ProyectoMetaversoApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.proyecto_metaverso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMetaversoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMetaversoApplication.class, args);
+	}
+
+}
